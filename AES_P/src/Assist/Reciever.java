c9581769f4aa5df1288ad11ad8b76fb12317c8f3
anikSourav1998@gmai.com <aniksourav1998@gmail.com>
2021-03-25 19:45:01 +0600

    init 2

---
+++ AES_P/src/Assist/Reciever.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assist;
+
+/**
+ *
+ * @author Anik Sourav
+ */
+public class Reciever implements Runnable{
+    int portID;
+    
+    @Override
+    public void run()
+    {
+        //asda
+    }
+    
+}
--- AES_P/src/Process/Process.java
+++ AES_P/src/Process/Process.java
@@ -3,19 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package aes_p;
+package Process;
 
 /**
  *
  * @author Anik Sourav
  */
-public class AES_P {
+public class Process implements Runnable{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    @Override
+    public void run() {
+        System.out.println("Process Running ");
     }
     
 }
