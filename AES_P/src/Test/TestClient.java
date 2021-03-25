5feeb1c9a913d1a2705af12835b1846cae47bed5
anikSourav1998@gmai.com <aniksourav1998@gmail.com>
2021-03-25 23:46:37 +0600

    Test1 Complete

--- AES_P/src/Test/TestClient.java
+++ AES_P/src/Test/TestClient.java
@@ -5,10 +5,38 @@
  */
 package Test;
 
+import java.io.IOException;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+
 /**
  *
  * @author Anik Sourav
  */
 public class TestClient {
+    public static void main(String[] args) throws IOException{
+        Socket clientSocket = new Socket("localhost", 9999);
+        
+        ObjectOutputStream oStream = new ObjectOutputStream(clientSocket.getOutputStream());
+        ObjectInputStream iStream = new ObjectInputStream(clientSocket.getInputStream());
+        
+        Scanner in=new Scanner(System.in);
+        String message=in.nextLine();
+        
+        oStream.writeObject(message);
+        
+        
+        try {
+            Object recieved = iStream.readObject();
+            System.out.println("From Server"+ (String) recieved );
+            
+        } catch (Exception e) {
+            System.out.println("Test.TestClient.main()");
+        }
+        
+        
+    }
     
 }
--- AES_P/src/Test/TestServer.java
+++ AES_P/src/Test/TestServer.java
@@ -5,10 +5,32 @@
  */
 package Test;
 
+
+import java.io.IOException;
+import java.io.*;
+import java.net.*;
+
 /**
  *
  * @author Anik Sourav
  */
 public class TestServer {
-    
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSocket =  new ServerSocket(9999);
+        Socket sock = serverSocket.accept();
+        
+        ObjectOutputStream oStream = new ObjectOutputStream(sock.getOutputStream());
+        ObjectInputStream iStream = new ObjectInputStream(sock.getInputStream());
+        
+        
+        try {
+            Object recieved = iStream.readObject();
+            System.out.println("Recieved"+(String) recieved);
+            
+            oStream.writeObject("Sent From Server");
+            
+        } catch (Exception e) {
+            System.out.println("Test.TestServer.main()");
+        }
+    }
 }
